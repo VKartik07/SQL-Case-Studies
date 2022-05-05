6706ebeaaecdcbcb1eb7339b199ce966c5fb3192
VKartik07 <vinodkartiknarain@gmail.com>
2022-05-06 00:07:27 +0530

    Modified: Comments

--- Week-2/Week_2_Cleaning.sql
+++ Week-2/Week_2_Cleaning.sql
@@ -1,78 +1,59 @@
---SELECT *
---FROM Eight_Week_Challenge_2.dbo.customer_orders; 
+-- NULL Handling [Customer Orders Table]
 
--- NULL Handling
+--Replace null values (strings) with empty strings
 
--- Replace null values (strings) with empty strings
+UPDATE Eight_Week_Challenge_2.dbo.customer_orders
+	SET 
+		exclusions = REPLACE(exclusions, 'null', ''), 
+		extras = REPLACE(extras, 'null', ''); 
 
---UPDATE Eight_Week_Challenge_2.dbo.customer_orders
---	SET 
---		exclusions = REPLACE(exclusions, 'null', ''), 
---		extras = REPLACE(extras, 'null', ''); 
+--Replace empty strings with NULL
 
--- Replace empty strings with NULL
+UPDATE Eight_Week_Challenge_2.dbo.customer_orders
+	SET
+		exclusions = NULLIF(exclusions, ''),
+		extras = NULLIF(extras, '');
 
---UPDATE Eight_Week_Challenge_2.dbo.customer_orders
---	SET
---		exclusions = NULLIF(exclusions, ''),
---		extras = NULLIF(extras, '');
-
-
---SELECT *
---FROM Eight_Week_Challenge_2.dbo.pizza_names; 
-
---SELECT *
---FROM Eight_Week_Challenge_2.dbo.pizza_recipes; 
-
---SELECT *
---FROM Eight_Week_Challenge_2.dbo.pizza_toppings; 
-
---SELECT *
---FROM Eight_Week_Challenge_2.dbo.runner_orders; 
-
--- NULL Handling
+-- NULL Handling [Runner Orders Table]
 
 -- Replace null values (strings) with empty strings
 
---UPDATE Eight_Week_Challenge_2.dbo.runner_orders
---	SET 
---		cancellation = REPLACE(cancellation, 'null', ''), 
---		distance = REPLACE(distance, 'null', ''),
---		duration = REPLACE(duration, 'null', '');
---		pickup_time = REPLACE(pickup_time, 'null', '')
+UPDATE Eight_Week_Challenge_2.dbo.runner_orders
+	SET 
+		cancellation = REPLACE(cancellation, 'null', ''), 
+		distance = REPLACE(distance, 'null', ''),
+		duration = REPLACE(duration, 'null', '');
+		pickup_time = REPLACE(pickup_time, 'null', '')
 		
 -- Replace empty strings with NULL
 
---UPDATE Eight_Week_Challenge_2.dbo.runner_orders
---	SET
---		distance = NULLIF(distance, ''),
---		duration = NULLIF(duration, ''), 
---		cancellation = NULLIF(cancellation, ''); 
---		pickup_time = NULLIF(pickup_time, '');
+UPDATE Eight_Week_Challenge_2.dbo.runner_orders
+	SET
+		distance = NULLIF(distance, ''),
+		duration = NULLIF(duration, ''), 
+		cancellation = NULLIF(cancellation, ''); 
+		pickup_time = NULLIF(pickup_time, '');
 
 -- Values Inconsistency Handling
 
---UPDATE Eight_Week_Challenge_2.dbo.runner_orders
---SET 
---	distance = 
---		CASE 
---			WHEN CHARINDEX('k', distance) > 0
---				THEN 
---					REPLACE(distance, SUBSTRING(distance, CHARINDEX('k', distance), (LEN(distance) - CHARINDEX('k', distance))+1), '')
---			ELSE
---				distance
---		END 	
+UPDATE Eight_Week_Challenge_2.dbo.runner_orders
+SET 
+	distance = 
+		CASE 
+			WHEN CHARINDEX('k', distance) > 0
+				THEN 
+					REPLACE(distance, SUBSTRING(distance, CHARINDEX('k', distance), (LEN(distance) - CHARINDEX('k', distance))+1), '')
+			ELSE
+				distance
+		END 	
 
---UPDATE Eight_Week_Challenge_2.dbo.runner_orders
---SET 
---	duration = 
---		CASE 
---			WHEN CHARINDEX('m', duration) > 0
---				THEN 
---					REPLACE(duration, SUBSTRING(duration, CHARINDEX('m', duration), (LEN(duration) - CHARINDEX('m', duration))+1), '')
---			ELSE
---				duration
---		END 
-
---SELECT *
---FROM Eight_Week_Challenge_2.dbo.runners;
+UPDATE Eight_Week_Challenge_2.dbo.runner_orders
+SET 
+	duration = 
+		CASE 
+			WHEN CHARINDEX('m', duration) > 0
+				THEN 
+					REPLACE(duration, SUBSTRING(duration, CHARINDEX('m', duration), (LEN(duration) - CHARINDEX('m', duration))+1), '')
+			ELSE
+				duration
+		END 
